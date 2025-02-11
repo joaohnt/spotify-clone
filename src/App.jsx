5b206eeb620934c608af3c017592a95f1a098b47
joaohnt <jpedrooo.cg@gmail.com>
2025-02-10 23:03:51 -0300

    estilizando com css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './compenents/Header'
+import Main from './compenents/Main';
+
+const App = () => {
+  return (
+  <>
+    <Header/>
+    <Main/>
+  </>
+  )
+}
+
+export default App
---
+++ src/compenents/Main.jsx
@@ -0,0 +1,290 @@
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='item-list'>
+                <div className='item-list__header'>
+                    <h2>Artistas populares</h2>
+                    <a className='item-list__link' href='/'>
+                        Mostrar tudo
+                    </a>
+                </div>
+
+                <div className='item-list__container'>
+
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <div className=' single-item__title'>
+                                <p className='single-item__title'>aespa</p>
+                            </div>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+            <div className='item-list'>
+                <div className='item-list__header'>
+                    <h2>Músicas populares</h2>
+                    <a className='item-list__link' href='/'>
+                        Mostrar tudo
+                    </a>
+                </div>
+
+                <div className='item-list__container'>
+
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <div className=' single-item__title'>
+                                <p className='single-item__title'>aespa</p>
+                            </div>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <div className=' single-item__title'>
+                                <p className='single-item__title'>aespa</p>
+                            </div>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+                    <div className='single-item'>
+                        <div className='single-item__div-image-button'>
+
+                            <div className='single-item__div-image'>
+                                <img className="single-item__image" src="https://i.scdn.co/image/ab6761610000e5ebf7a1090ad3a35a34fc0ecb57" alt="Imagem do artista X" />
+                                <FontAwesomeIcon
+                                    className='single-item__icon'
+                                    icon={faCirclePlay} />
+                            </div>
+                        </div>
+                        <div className='single-item__texts'>
+                            <p className='single-item__title'>aespa</p>
+                            <p className='single-item__type'>Artista</p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
