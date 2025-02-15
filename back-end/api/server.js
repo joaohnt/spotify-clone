5244f3eb5aa387be39717657d88019c5beafd062
joaohnt <jpedrooo.cg@gmail.com>
2025-02-15 02:50:22 -0300

    deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { db } from "./connect.js";
 import cors from "cors";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -8,14 +11,20 @@ const PORT = 3000;
 app.use(cors());
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("/artists e /songs");
 });
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- back-end/api/tempCodeRunnerFile.js
+++ back-end/api/tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-console.log(newArtistArray);
+import { MongoClient } from "mongodb";
+
+const URI =
+  "mongodb+srv://jpedrooocg:X9Xf0uLN631B9JiT@spotify.3vzrs.mongodb.net/?retryWrites=true&w=majority&appName=spotify";
+
+const client = new MongoClient(URI);
+
+export const db = client.db("spotify");
+
+//const songCollection = await db.collection("songs").find({}).toArray();
+console.log(db);
