4dde22dfefab0b10765aa1a314a29909c916db65
joaohnt <jpedrooo.cg@gmail.com>
2025-02-15 03:06:57 -0300

    process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://spotify-clone-a4b3.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
